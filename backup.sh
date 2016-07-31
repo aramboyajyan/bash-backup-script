536ea8dbef5ec5ce84f2da1df6c193ae566d6a5f
Aram Boyajyan <dev@aram.cz>
2016-07-31 20:02:36 +0200

    fixes #6; removing clear

--- backup.sh
+++ backup.sh
@@ -9,9 +9,13 @@
 # Load our configuration file.
 script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$script_directory/backup.cfg"
+output_destination="$backup_directory/$project_name-$date-output.txt"
+
+# Add the "header" in output file.
+echo "Generated on: $date" > $output_destination
 
 # Zip the directory.
-zip -r "$backup_directory/$project_name-$date-files.zip" $website
+zip -r "$backup_directory/$project_name-$date-files.zip" $website >> "$output_destination"
 
 # Backup the database.
 if [ -z ${db_user+x} ] && [ -z ${db_pass+x} ]
@@ -26,13 +30,17 @@ else
 fi
 
 # Zip the database dump and remove the uncompressed file once done.
-zip "$backup_directory/$project_name-$date-database.zip" "$backup_directory/$project_name-$date-database.sql"
+zip "$backup_directory/$project_name-$date-database.zip" "$backup_directory/$project_name-$date-database.sql" >> "$output_destination"
 rm "$backup_directory/$project_name-$date-database.sql"
 
+# Do the same with the output file.
+zip "$backup_directory/$project_name-$date-output.zip" "$output_destination" > /dev/null 2>&1
+rm "$output_destination"
+
 # Done!
-clear
 echo '********************************************************************************'
 echo 'Successfully backed up the files and the database to:'
 echo "    $backup_directory/$project_name-$date-database.zip"
 echo "    $backup_directory/$project_name-$date-files.zip"
+echo "    $backup_directory/$project_name-$date-output.zip"
 echo '********************************************************************************'
